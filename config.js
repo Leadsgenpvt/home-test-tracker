b104322b0195a54875e55337553d23e17155327a
Leadsgenpvt <leadsgenpvt@gmail.com>
2026-09-11 18:59:19 +0000

    Update admin and technician passwords

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   admin: {
     username: process.env.ADMIN_USER || 'owner',
-    password: process.env.ADMIN_PASSWORD || 'changeme123'
+    password: process.env.ADMIN_PASSWORD || 'Falcon@Track92'
   },
   technician: {
     username: process.env.TECH_USER || 'technician',
-    password: process.env.TECH_PASSWORD || 'changeme123'
+    password: process.env.TECH_PASSWORD || 'Falcon@Route47'
   }
 };
